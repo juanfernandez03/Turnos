a1202ff3e39585f493eeeec2fcd8ba5062a4faaf
Juan Jose Fernandez <YaYoo_13@hotmail.com>
2022-01-04 17:41:07 -0300

    se agrega el proyecto

---
+++ Controllers/EspecialidadController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Turnos.Controllers
+{
+    public class EspecialidadController : Controller 
+    {
+        
+        public EspecialidadController()
+
+    {
+
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    }
+}
